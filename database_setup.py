3978f131a2ce25b9446dc22fdd21e136748a9422
oramirezperera <ramirez.orlando17@gmail.com>
2024-09-23 14:59:24 -0400

    created the structure for a first table

--- database_setup.py
+++ database_setup.py
@@ -1,1 +1,18 @@
 ''' Creating a file to create the DB '''
+#importing libraries
+import sqlite3
+
+#connecting the Database
+conn = sqlite3.connect('supplies.db')
+cursor = conn.cursor()
+
+# Creating the supplies table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS supplies(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT,
+    QUANTITY INTEGER NOT NULL,
+    price_per_unit REAL NOT NULL
+);
+''')
