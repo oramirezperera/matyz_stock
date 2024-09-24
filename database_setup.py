49150a1791d8b6a87edd04ac7db6e4aaf84ce338
oramirezperera <ramirez.orlando17@gmail.com>
2024-09-24 12:56:04 -0400

    added some changes to the tables and finalized the setup of the DB

--- database_setup.py
+++ database_setup.py
@@ -8,11 +8,43 @@ cursor = conn.cursor()
 
 # Creating the supplies table
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS supplies(
+CREATE TABLE IF NOT EXISTS products(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     description TEXT,
     QUANTITY INTEGER NOT NULL,
-    price_per_unit REAL NOT NULL
+    price_per_unit REAL NOT NULL,
+    category TEXT NOT NULL,
+    brand TEXT NOT NULL
 );
 ''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS customers(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT,
+    phone TEXT,
+    tattoo_tag TEXT,
+    ig_account TEXT
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS sales(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_id INTEGER,
+    quantity_sold INTEGER NOT NULL,
+    sale_date DATE DEFAULT (DATE('now')),
+    customer_id INTEGER,
+    FOREIGN KEY (product_id) REFERENCES products (id)
+    FOREIGN KEY (customer_id) REFERENCES customers (id)
+);
+'''
+)
+
+# Commit Changes and close
+conn.commit()
+conn.close()
+
+print("Database and tables created successfully")
