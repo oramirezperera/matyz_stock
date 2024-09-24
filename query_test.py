b004db0211d7a363ca2a45fd9d3d179eff1fcbed
oramirezperera <ramirez.orlando17@gmail.com>
2024-09-24 16:17:04 -0400

    created a sample query for testing

---
+++ query_test.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+conn = conn.connect('tatoo_supplies.db')
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM supplies")
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+conn.close()
